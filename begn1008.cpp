c84484f421ddce047dae2a28eaea7cb0fb8d202d
Muntaha2427 <muntahamazumder612@gmail.com>
2025-09-26 02:05:42 +0600

    Task9

---
+++ begn1008.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num,whm;
+    double amn;
+    cin>>num>>whm>>amn;
+    cout<<"NUMBER = "<<num<<endl;
+    cout<<fixed<<setprecision(2)<<"SALARY = U$ "<<whm*amn<<endl;
+    return 0;
+}
