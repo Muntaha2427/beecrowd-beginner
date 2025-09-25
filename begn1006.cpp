e322887f433bc5b68d311087551f433b8a33e674
Muntaha2427 <muntahamazumder612@gmail.com>
2025-09-26 02:00:20 +0600

    HomeWork

---
+++ begn1006.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double A,B,C,avg;
+    cin>>A>>B>>C;
+    avg=(A*2 + B*3 + C*5)/(2+3+5);
+    cout<<fixed<<setprecision(1)<<"MEDIA = "<<avg<<endl;
+    return 0;
+}
