df0c6aacd4b88d0f1c5fd5074f664ad9d0bfea6c
Muntaha2427 <muntahamazumder612@gmail.com>
2025-10-08 10:25:09 +0600

    Task 14

---
+++ begn 1010.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b,c,d;
+    double e,f;
+    cin>>a>>b>>c;
+    cin>>d>>e>>f;
+    cout<<"Valor a pagar: R$ "<<(b*c+e*f)<<endl;
+    return 0;
+}
